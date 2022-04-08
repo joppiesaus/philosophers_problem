7b244680e57d2c41173702f0895a13ac7af57cda
Job van der Zweep <jobvan-d@student.codam.nl>
2022-04-08 16:44:13 +0200

    code structure experiments

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,75 @@
 /*   By: jobvan-d <jobvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/15 17:55:03 by jobvan-d      #+#    #+#                 */
-/*   Updated: 2022/02/15 18:07:52 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/04/08 16:43:01 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phil.h"
 
+#include <stdlib.h> /* malloc */
+
+t_philosopher	*init_guests(uint32_t amount)
+{
+	t_philosopher	*guests;
+	uint32_t		i;
+
+	guests = malloc(sizeof(t_philosopher) * amount);
+	if (guests)
+	{
+		i = 0;
+		while (i < amount)
+		{
+			guests[i].number = i + 1;
+			i++;
+		}
+	}
+	return (guests);
+}
+
+t_fork	*init_forks(uint32_t amount)
+{
+	t_fork		*forks;
+	uint32_t	i;
+
+	forks = malloc(sizeof(t_fork) * amount);
+	if (forks)
+	{
+		i = 0;
+		while (i < amount)
+		{
+			if (pthread_mutex_init(&forks[i].mutex, NULL) == -1)
+			{
+				free(forks);
+				return (NULL);
+			}
+			i++;
+		}
+	}
+	return (forks);
+}
+
+/* initializes a table with philosophers and forks.
+ * no threading yet. Returns 1 on success, 0 on malloc fail. */
+int	init_table(t_table *table, uint32_t population)
+{
+	table->population = population;
+	table->guests = init_guests(population);
+	if (!table->guests)
+		return (0);
+	table->forks = init_forks(population);
+	if (!table->forks)
+		return (0);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
+	t_table	table;
+
+	if (!init_table(&table, 5))
+	{
+		return (1);
+	}
 	return (0);
 }
--- philo/phil.h
+++ philo/phil.h
@@ -1,16 +1,51 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   philo.h                                            :+:    :+:            */
+/*   phil.h                                             :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: jobvan-d <jobvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/15 17:56:47 by jobvan-d      #+#    #+#                 */
-/*   Updated: 2022/02/15 17:57:15 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/04/08 16:43:18 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHIL_H
 # define PHIL_H
+
+# include <pthread.h>
+# include <stdint.h>
+
+typedef struct s_fork
+{
+	pthread_mutex_t	mutex;
+}	t_fork;
+
+typedef struct s_philosopher
+{
+	uint32_t	number;
+}	t_philosopher;
+
+typedef struct s_table
+{
+	t_philosopher	*guests;
+	t_fork			*forks;
+	uint32_t		population;
+}	t_table;
+
+int				init_table(t_table *table, uint32_t population);
+t_philosopher	*init_guests(uint32_t amount);
+t_fork			*init_forks(uint32_t amount);
+
+t_fork			*get_left_fork(t_table *table, t_philosopher *phil);
+t_fork			*get_right_fork(t_table *table, t_philosopher *phil);
+
+void			take_fork(t_fork *fork);
+void			return_fork(t_fork *fork);
+
+void			eat(t_philosopher *phil);
+void			think(t_philosopher *phil);
+void			sleep(t_philosopher *phil);
+void			starve(t_philosopher *phil);
 
 #endif
