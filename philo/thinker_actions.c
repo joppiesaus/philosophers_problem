7181b9b4fcc2dc30b070ffab17e004ccffcaa2dd
Job van der Zweep <jobvan-d@student.codam.nl>
2022-04-12 15:54:43 +0200

    fix thinking/sleeping swapped around

--- philo/thinker_actions.c
+++ philo/thinker_actions.c
@@ -6,7 +6,7 @@
 /*   By: jobvan-d <jobvan-d@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/12 14:54:14 by jobvan-d      #+#    #+#                 */
-/*   Updated: 2022/04/12 15:39:51 by jobvan-d      ########   odam.nl         */
+/*   Updated: 2022/04/12 15:53:22 by jobvan-d      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,13 @@ void	eat(t_thinker *thinker)
 
 void	think(t_thinker *thinker)
 {
-	thinker_print_msg(thinker, "is sleeping");
-	msleep(thinker->vars->time_to_sleep);
+	thinker_print_msg(thinker, "is thinking");
 }
 
 void	thinker_sleep(t_thinker *thinker)
 {
-	thinker_print_msg(thinker, "is thinking");
+	thinker_print_msg(thinker, "is sleeping");
+	msleep(thinker->vars->time_to_sleep);
 }
 
 void	starve(t_thinker *thinker)
